2a17ce9c614b8b534bd4aad32c61b6d980d4f961
amine <zazamine8@gmail.com>
2019-11-28 03:23:06 +0000

    prepare vpn function which will intialize the vpn connection

--- android/src/main/java/com/reactnative/capturetraffic/CaptureTraffic.java
+++ android/src/main/java/com/reactnative/capturetraffic/CaptureTraffic.java
@@ -1,15 +1,20 @@
 package com.reactnative.capturetraffic;
 
+import android.app.Activity;
+import android.content.Intent;
+
+import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.github.megatronking.netbare.NetBare;
 import com.github.megatronking.netbare.ssl.JKS;
 
 import java.io.IOException;
 
-public class CaptureTraffic {
+public class CaptureTraffic implements ActivityEventListener {
 
 
     private static final String TAG = CaptureTraffic.class.getSimpleName();
+    private static final int REQUEST_CODE_PREPARE = 1;
 
     private final ReactApplicationContext reactContext;
     private final JKS mJKS;
@@ -21,6 +26,7 @@ public class CaptureTraffic {
         this.mNetBare = mNetBare;
         this.mCertificate = certificate;
 
+        this.reactContext.addActivityEventListener(this);
         mJKS = new JKS(context, certificate.getAlias(), certificate.getPassword().toCharArray(), certificate.getCommonName(), certificate.getOrganization(),
                 certificate.getOrganizationalUnitName(), certificate.getCertOrganization(), certificate.getCertOrganizationalUnitName());
     }
@@ -36,5 +42,20 @@ public class CaptureTraffic {
             }
         }else
             return true;
+    }
+
+    public void prepareVpn(){
+        Intent intent = this.mNetBare.prepare();
+        if (intent != null) {
+            reactContext.startActivityForResult(intent, REQUEST_CODE_PREPARE, null);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        this.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_PREPARE) {
+            prepareVpn();
+        }
     }
 }
