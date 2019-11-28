cfe8d01e8539b512268489ec3d81738445bd68bc
amine <zazamine8@gmail.com>
2019-11-28 02:41:36 +0000

    the main module that will be exported in JS side

--- android/src/main/java/com/reactnative/capturetraffic/RNCaptureTrafficModule.java
+++ android/src/main/java/com/reactnative/capturetraffic/RNCaptureTrafficModule.java
@@ -1,22 +1,32 @@
-
 package com.reactnative.capturetraffic;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.github.megatronking.netbare.NetBare;
 
 public class RNCaptureTrafficModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
 
+  private static final String TAG = RNCaptureTrafficModule.class.getSimpleName();
+  private final NetBare mNetBare;
+
   public RNCaptureTrafficModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+
+    mNetBare = NetBare.get();
   }
 
   @Override
   public String getName() {
-    return "RNCaptureTraffic";
+    return TAG;
+  }
+
+  @Override
+  public void initialize() {
+    super.initialize();
+
+    this.mNetBare.attachApplication(this.reactContext, BuildConfig.DEBUG);
   }
 }
