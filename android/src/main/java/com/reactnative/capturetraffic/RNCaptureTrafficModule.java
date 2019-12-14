305b819982418957b7f5a0eb3fc3c4861adb7a94
amine <zazamine8@gmail.com>
2019-12-14 04:02:51 +0000

    expose CaptureTraffic methods to React

--- android/src/main/java/com/reactnative/capturetraffic/RNCaptureTrafficModule.java
+++ android/src/main/java/com/reactnative/capturetraffic/RNCaptureTrafficModule.java
@@ -1,8 +1,9 @@
 package com.reactnative.capturetraffic;
 
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -14,84 +15,91 @@ import com.github.megatronking.netbare.NetBareListener;
 
 public class RNCaptureTrafficModule extends ReactContextBaseJavaModule implements NetBareListener {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  private static final String TAG = RNCaptureTrafficModule.class.getSimpleName();
-  private final NetBare mNetBare;
-  private CaptureTraffic mCaptureTraffic;
+    private static final String TAG = RNCaptureTrafficModule.class.getSimpleName();
+    private final NetBare mNetBare;
+    private Certificate cert;
+    private CaptureTraffic mCaptureTraffic;
 
-  public RNCaptureTrafficModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
+    public RNCaptureTrafficModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
 
-    mNetBare = NetBare.get();
-  }
+        mNetBare = NetBare.get();
+    }
 
-  @Override
-  public String getName() {
-    return TAG;
-  }
+    @Override
+    public String getName() {
+        return "RNCaptureTraffic";
+    }
 
-  @Override
-  public void initialize() {
-    super.initialize();
+    @Override
+    public void initialize() {
+        super.initialize();
 
-    this.mNetBare.attachApplication(this.reactContext, BuildConfig.DEBUG);
-    this.mNetBare.registerNetBareListener(this);
-  }
+        this.mNetBare.attachApplication(this.reactContext, BuildConfig.DEBUG);
+        this.mNetBare.registerNetBareListener(this);
+    }
 
-  @Override
-  public void onCatalystInstanceDestroy() {
-    super.onCatalystInstanceDestroy();
-    this.mNetBare.unregisterNetBareListener(this);
-    this.mNetBare.stop();
-    this.mCaptureTraffic.destroy();
-  }
+    @Override
+    public void onCatalystInstanceDestroy() {
+        super.onCatalystInstanceDestroy();
+        this.mNetBare.unregisterNetBareListener(this);
+        this.mNetBare.stop();
+        this.mCaptureTraffic.destroy();
+    }
 
-  private void sendEvent(ReactContext reactContext,
-                         String eventName,
-                         @Nullable WritableMap params) {
-    reactContext
-            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-            .emit(eventName, params);
-  }
+    private void sendEvent(ReactContext reactContext,
+                           String eventName,
+                           @Nullable WritableMap params) {
+        reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
 
-  @ReactMethod
-  public void setCertificate(String mAlias, String mPassword, String mCommonName, String mOrganization, String mOrganizationalUnitName,
-                             String mCertOrganization, String mCertOrganizationalUnitName) {
-    Certificate cert = new Certificate()
-            .setAlias(mAlias)
-            .setPassword(mPassword)
-            .setCommonName(mCommonName)
-            .setOrganization(mOrganization)
-            .setOrganizationalUnitName(mOrganizationalUnitName)
-            .setCertOrganization(mCertOrganization)
-            .setCertOrganizationalUnitName(mCertOrganizationalUnitName);
-    mCaptureTraffic = new CaptureTraffic(this.reactContext, this.mNetBare, cert);
-  }
+    @ReactMethod
+    public void setCertificate(String mAlias, String mPassword, String mCommonName, String mOrganization, String mOrganizationalUnitName,
+                               String mCertOrganization, String mCertOrganizationalUnitName) {
+        this.cert = new Certificate()
+                .setAlias(mAlias)
+                .setPassword(mPassword)
+                .setCommonName(mCommonName)
+                .setOrganization(mOrganization)
+                .setOrganizationalUnitName(mOrganizationalUnitName)
+                .setCertOrganization(mCertOrganization)
+                .setCertOrganizationalUnitName(mCertOrganizationalUnitName);
 
-  @ReactMethod
-  public void installCertificate(Callback success, Callback error){
-    mCaptureTraffic.installCertificate(success, error);
-  }
+    }
 
-  @ReactMethod
-  public void prepareVpn(){
-    mCaptureTraffic.prepareVpn();
-  }
+    @ReactMethod
+    public void installCertificate(final Promise promise){
+        mCaptureTraffic = new CaptureTraffic(this.reactContext, this.mNetBare, this.cert);
+        mCaptureTraffic.installCertificate(promise);
+    }
 
-  @ReactMethod
-  public void startVpn(){
-    mCaptureTraffic.startVpn();
-  }
+    @ReactMethod
+    public void startVpn(final Promise promise){
+        mCaptureTraffic.startVpn(promise);
+    }
 
-  @Override
-  public void onServiceStarted() {
-    sendEvent(this.reactContext, "onServiceStarted", null);
-  }
+    @ReactMethod
+    public void stopVpn(final Promise promise){
+        mCaptureTraffic.stopVpn(promise);
+    }
 
-  @Override
-  public void onServiceStopped() {
-    sendEvent(this.reactContext, "onServiceStopped", null);
-  }
+    @ReactMethod
+    public void isActive(final Promise promise){
+        promise.resolve(mNetBare.isActive());
+    }
+
+    @Override
+    public void onServiceStarted() {
+        sendEvent(this.reactContext, "onServiceStarted", null);
+    }
+
+    @Override
+    public void onServiceStopped() {
+        sendEvent(this.reactContext, "onServiceStopped", null);
+    }
 }
